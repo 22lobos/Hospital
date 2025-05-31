7884f8bfc851884f11cbf76273f985e2b448123f
22lobos <cr.loboso@duocuc.cl>
2025-05-31 19:02:05 -0400

    algunos poblado de comentarios

--- src/main/java/com/hospital_vm/cl/hopital_vm/model/Paciente.java
+++ src/main/java/com/hospital_vm/cl/hopital_vm/model/Paciente.java
@@ -5,24 +5,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "paciente")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.Date;
+
+
+@Entity // es la entidad de la tabla 
+@Table(name = "paciente") // nombre de la tabla en la base de datos
+@Data // Lombok para generar getters, setters, toString, equals y hashCode
+@AllArgsConstructor  // Lombok para generar un constructor con todos los argumentos
+@NoArgsConstructor // Lombok para generar un constructor sin argumentos
 
 public class Paciente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id // Indica que este campo es la clave primaria de la entidad
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // Generación automática del ID por la base de datos
+    private Long id;// Identificador único del paciente            
     
-    @Column(unique=true, length = 13 , nullable = false)
-    private String run;
+    @Column(unique=true, length = 13 , nullable = false) // Columna para el RUN del paciente, único y no nulo
+    private String run ;  // RUN del paciente, debe ser único y no nulo              
 
     @Column(nullable = false)
     private String nombre;
 
-    @Column(nullable = false)
+    @Column(nullable = false) // Columna para el apellido del paciente, no nulo
     private String apellido;
 
     @Column(nullable = true)
