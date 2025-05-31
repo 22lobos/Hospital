d649a714c9876196c50734107cf9db516d0c1a2f
22lobos <cr.loboso@duocuc.cl>
2025-05-31 19:28:04 -0400

    uso de @query

--- src/main/java/com/hospital_vm/cl/hopital_vm/repository/PacienteRepository.java
+++ src/main/java/com/hospital_vm/cl/hopital_vm/repository/PacienteRepository.java
@@ -2,6 +2,9 @@ package com.hospital_vm.cl.hopital_vm.repository;
 
 
 import com.hospital_vm.cl.hopital_vm.model.Paciente;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +17,11 @@ import org.springframework.stereotype.Repository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     
-    
+    List<Paciente> findByApellido(String pellidos);
+
+    Paciente findByRun(String correo);
+
+    List<Paciente> finByNombreApellidos(String nombre, String apellidos);
 
     
 }
