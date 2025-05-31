ca62ee135a23b351aa7228255b5a385f2907666b
22lobos <cr.loboso@duocuc.cl>
2025-05-31 17:33:36 -0400

    relleno de la clase paciente

--- src/main/java/com/hospital_vm/cl/hopital_vm/model/Paciente.java
+++ src/main/java/com/hospital_vm/cl/hopital_vm/model/Paciente.java
@@ -1,5 +1,37 @@
 package com.hospital_vm.cl.hopital_vm.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "paciente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(unique=true, length = 13 , nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = true)
+    private String  fechaNscimiento;
+
+    @Column(nullable = false)
+    private String correo;
+
+
+
+
 }
