13df05fa21c320ec4ab2607fd588bc0b063acc9f
22lobos <cr.loboso@duocuc.cl>
2025-05-31 19:45:05 -0400

    uso de query

--- src/main/java/com/hospital_vm/cl/hopital_vm/repository/PacienteRepository.java
+++ src/main/java/com/hospital_vm/cl/hopital_vm/repository/PacienteRepository.java
@@ -6,6 +6,8 @@ import com.hospital_vm.cl.hopital_vm.model.Paciente;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -17,11 +19,14 @@ import org.springframework.stereotype.Repository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     
-    List<Paciente> findByApellido(String pellidos);
+    @Query("SELECT p FROM Paciente p WHERE p.nombre = :apellido")
+    List<Paciente> buscarPorApellidos(@Param("apellidos") String apellido);
+ 
 
-    Paciente findByRun(String correo);
+    @Query(value = " FROM PACIENTE WHERE correo = correo", nativeQuery = true)
+    Paciente buscaPorCorreo(@param ("correo") String correo);
 
-    List<Paciente> finByNombreApellidos(String nombre, String apellidos);
+    List<Paciente> finByNombreApellidos(String nombre, String apellidos); 
 
     
 }
