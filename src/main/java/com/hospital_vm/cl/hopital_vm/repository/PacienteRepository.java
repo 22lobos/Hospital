7d38a50b65cd1cc523db85c9a5af70fabbc32a3c
22lobos <cr.loboso@duocuc.cl>
2025-05-31 19:18:23 -0400

    creacion de pacienterepository

---
+++ src/main/java/com/hospital_vm/cl/hopital_vm/repository/PacienteRepository.java
@@ -0,0 +1,22 @@
+package com.hospital_vm.cl.hopital_vm.repository;
+
+
+import com.hospital_vm.cl.hopital_vm.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+
+
+
+
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    
+    
+
+    
+}
+    
+
