15a55dc9842e728d1e2fd9c72612156ec8d09333
22lobos <cr.loboso@duocuc.cl>
2025-05-31 20:01:28 -0400

    importacion

--- src/main/java/com/hospital_vm/cl/hopital_vm/service/PacienteService.java
+++ src/main/java/com/hospital_vm/cl/hopital_vm/service/PacienteService.java
@@ -1,5 +1,17 @@
 package com.hospital_vm.cl.hopital_vm.service;
 
+
+
+import com.hospital_vm.cl.hopital_vm.model.Paciente;
+import com.hospital_vm.cl.hopital_vm.repository.PacienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+
 public class PacienteService {
     
 }
